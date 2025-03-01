1dd1caa28b0f8914ca1530e2460d79cfcb73dd64
Cristhian T <cristhian.torres1@unmsm.edu.pe>
2025-02-28 23:41:55 -0500

    test

--- src/db.ts
+++ src/db.ts
@@ -1,34 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
-import { QueryResult, QueryResultRow } from 'pg';
+import { Pool, QueryResult, QueryResultRow } from 'pg';
 
-// Inicializa Supabase
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const pool = new Pool({
+  user: process.env.NEXT_PUBLIC_DB_USER as string, 
+  host: process.env.NEXT_PUBLIC_DB_HOST as string,
+  database: process.env.NEXT_PUBLIC_DB_NAME as string,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD as string,
+  port: Number(process.env.NEXT_PUBLIC_DB_PORT), 
+});
 
-// Función para realizar consultas
-export const query = async <T extends QueryResultRow = any>(
-  text: string,
-  params?: unknown[]
-): Promise<QueryResult<T>> => {
-  try {
-    // Llama a la función RPC con el nombre y los parámetros
-    const { data, error } = await supabase.rpc(text, params || []);
-
-    if (error) {
-      throw error; // Lanza el error si hay problemas con la consulta
-    }
-
-    // Adapta la respuesta de Supabase al formato de QueryResult de pg
-    return {
-      rows: data as T[],               // Los datos obtenidos de la consulta
-      rowCount: (data as T[]).length,  // El conteo de las filas devueltas
-      fields: [],                      // Los campos no están disponibles, por lo que los dejamos vacíos
-      command: 'SELECT',               // Añadimos un valor predeterminado para 'command'
-      oid: 0                           // Añadimos un valor predeterminado para 'oid'
-    };
-  } catch (error) {
-    console.error('Error en la consulta:', error);
-    throw error; // Relanza el error para manejarlo en el código que llama a esta función
-  }
+export const query = async <T extends QueryResultRow=any>(text: string, params?: unknown[]): Promise<QueryResult<T>> => {
+  return pool.query<T>(text, params);
 };
