ae87a8e511dc8eab04c610f4184497332c707620
Cristhian T <cristhian.torres1@unmsm.edu.pe>
2025-02-28 23:27:25 -0500

    vercel issues

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@ const createNextIntlPlugin = require('next-intl/plugin')
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    runtime: 'nodejs',  // Cambiar el runtime a Node.js en lugar de Edge
+  },
+}
 
 module.exports = withNextIntl(nextConfig)
-
-module.exports = {
-    experimental: {
-      runtime: 'nodejs',  // Cambiar el runtime a Node.js en lugar de Edge
-    },
-  };
