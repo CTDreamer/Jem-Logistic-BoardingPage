61876a375f573b658c0d7b9e4a5f7eca4a41205e
Cristhian T <cristhian.torres1@unmsm.edu.pe>
2025-03-01 01:04:29 -0500

    db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,17 @@
 import { Pool, QueryResult, QueryResultRow } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  database: process.env.NEXT_PUBLIC_DB_NAME, // Corregido aquí
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+  port: parseInt(process.env.NEXT_PUBLIC_DB_PORT || '5432', 10), // Corregido la conversión de tipo
+  connectionString: process.env.NEXT_CONNECTION_STRING,
   ssl: {
-    rejectUnauthorized : false
+    rejectUnauthorized: false
   }
 });
 
-export const query = async <T extends QueryResultRow=any>(text: string, params?: unknown[]): Promise<QueryResult<T>> => {
+export const query = async <T extends QueryResultRow = any>(text: string, params?: unknown[]): Promise<QueryResult<T>> => {
   return pool.query<T>(text, params);
 };
