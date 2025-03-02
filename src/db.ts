d8b6e5308b739764151f7a22448248fb781d91ad
Cristhian T <cristhian.torres1@unmsm.edu.pe>
2025-03-01 19:17:27 -0500

    db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,6 @@
 import { Pool, QueryResult, QueryResultRow } from 'pg';
 
 const pool = new Pool({
-  user: process.env.NEXT_PUBLIC_DB_USER,
-  host: process.env.NEXT_PUBLIC_DB_HOST,
-  database: process.env.NEXT_PUBLIC_DB_NAME, // Corregido aquí
-  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-  port: parseInt(process.env.NEXT_PUBLIC_DB_PORT || '5432', 10), // Corregido la conversión de tipo
   connectionString: process.env.NEXT_CONNECTION_STRING,
   ssl: {
     rejectUnauthorized: false
